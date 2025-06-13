76a95ef9051c9eb0e2b0914d740b435200bbb393
iHarshMakwana <harsh.b.mak@gmail.com>
2025-06-13 14:28:11 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import HomePage from './pages/HomePage';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HomePage />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
